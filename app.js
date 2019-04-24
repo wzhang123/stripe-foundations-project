e2e536ad4ae5c679ea0041408e483b8a44c370b4
Weiheng Zhang <wzhang@stripe.com>
2019-04-23 17:45:11 -0700

    final webiste

--- app.js
+++ app.js
@@ -4,12 +4,14 @@ function purchaseProduct(event) {
   alert('hello world');
 }
 
-function openProductModal(title, description) {
+function openProductModal(title, description, price) {
   $('#purchaseProductModal').modal('show')
   let productTitle = document.querySelector('#productItemTitle')
   productTitle.innerHTML = title
   let productDes = document.querySelector('#productItemDescription')
   productDes.innerHTML = description
+  let productPri = document.querySelector('#productItemPrice')
+  productPri.innerHTML = price
 }
 
 console.log(event.srcElement.parentNode.parentNode.innerText)
